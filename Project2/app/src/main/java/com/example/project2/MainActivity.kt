24afee31bcf3c865e3f2cea3cb964688611ffd0c
Vitaly <aavuta.ru@mail.ru>
2020-05-06 01:37:44 +0700

    Я это сделаааааал

--- Project2/app/src/main/java/com/example/project2/MainActivity.kt
+++ Project2/app/src/main/java/com/example/project2/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.project2
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent as Intent1
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,42 +15,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myButton.setOnClickListener {
-            textView.text = "Hello Nastya!"
+
+
+    }
+
+    fun newActivityCamera(view: View) {
+        val newIntent = Intent1(this, SecondActivity::class.java).also {
+            it.putExtra(SecondActivity.TOTAL_KEY, true)
+            startActivity(it)
         }
+    }
 
+    fun newActivityGalery(view: View) {
+        val newIntent = Intent1(this, SecondActivity::class.java).also {
+            it.putExtra(SecondActivity.TOTAL_KEY, false)
+            startActivity(it)
+        }
     }
 
 
-    var count: Int = 0
 
-    fun newActivity(view: View){
-        val newIntent = Intent(this, SecondActivity::class.java)
 
-        newIntent.putExtra(SecondActivity.TOTAL_KEY, count)
-        startActivity(newIntent)
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     fun toastMe(view: View){
         var toast = Toast.makeText(this, "Хватит тыкать!", Toast.LENGTH_SHORT)
         toast.show()
     }
 
-    fun countMe(view: View){
-        count++
-        textView2.text = count.toString()
-    }
 
-    fun randMe(view:View){
-        count =  (1..12).shuffled().first().toInt()
-        textView2.text = count.toString()
-    }
-
-    fun clearMe(view: View){
-        //val countString = textView2.text.toString()
-        //var count: Int = Integer.parseInt(countString)
-        count = 0
-
-        textView2.text = count.toString()
-    }
+    //fun clearMe(view: View){
+    //count = 0
+    //textView2.text = count.toString()
+    //}
 }
--- Project2/app/src/main/java/com/example/project2/SecondActivity.kt
+++ Project2/app/src/main/java/com/example/project2/SecondActivity.kt
@@ -1,12 +1,21 @@
 package com.example.project2
 
+import android.app.Activity
 import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
+import androidx.core.graphics.createBitmap
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_second.*
+import java.io.File
 
 class SecondActivity : AppCompatActivity() {
 
@@ -14,27 +23,50 @@ class SecondActivity : AppCompatActivity() {
         const val TOTAL_KEY = "total_key"
     }
 
+    val REQUEST_IMAGE_CAPTURE = 1
+    val REQUEST_GALERY = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        showRandom()
+        var camera: Boolean = intent.getBooleanExtra(TOTAL_KEY, true)
+        if (camera) dispatchTakePictureIntentCamera()
+        else dispatchTakePictureIntentGalery()
     }
 
-    fun clickMe(view: View){
-        var toast = Toast.makeText(this, "Выйди от сюда, разбойник", Toast.LENGTH_SHORT)
-        toast.show()
+
+    private fun dispatchTakePictureIntentCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
     }
 
-    fun showRandom(){
-        val count = intent.getIntExtra(TOTAL_KEY, 0)
-        var randomInt = (0..count).shuffled().first().toInt()
+    private fun dispatchTakePictureIntentGalery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, REQUEST_GALERY)
+        }
+    }
 
-        randomTextView.text = Integer.toString(randomInt)
-        infoTextView.text = getString(R.string.interes_text, count)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val extras = data?.extras
+            val imageBitmap =
+                extras!!.get("data") as Bitmap?
+            imageView2.setImageBitmap(imageBitmap)
+        }
 
-
-
+        if(requestCode == REQUEST_GALERY && data!=null){
+            var imageUri: Uri = data.getData()!!
+            var imageStream = getContentResolver().openInputStream(imageUri)
+            var selectedImage = BitmapFactory.decodeStream(imageStream)
+            imageView2.setImageBitmap(selectedImage)
+        }
 
     }
 }
+
